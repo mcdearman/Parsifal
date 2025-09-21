aa54137160d6920f39fe6707337a6c1a3aa06fe7
mcdearman <mcdearmanchris@gmail.com>
2025-09-20 20:18:49 -0500

    update

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,33 @@
 module Main where
 
+import Data.ByteString (ByteString)
+import Data.Primitive (PrimArray, SmallArray)
+import Data.Word (Word16, Word8)
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+
+data GreenCtx = GreenCtx
+  { greenNodes :: !GreenNodes,
+    greenChildren :: !GreenChildren,
+    tokens :: !Tokens
+  }
+
+data GreenNodes = GreenNodes
+  { nodeKind :: !(PrimArray SyntaxKind),
+    nodeChildStart :: !(PrimArray Int),
+    nodeChildCount :: !(PrimArray Int),
+    nodeWidth :: !(PrimArray Int)
+  }
+
+data GreenChildren = GreenChildren
+  { childTag :: !(PrimArray Word8),
+    childIx :: !(PrimArray Int)
+  }
+
+data Tokens = Tokens
+  { tokKind :: !(PrimArray SyntaxKind),
+    tokText :: !(SmallArray ByteString)
+  }
+
+newtype SyntaxKind = SyntaxKind Word16 deriving (Show, Eq, Ord)
