d2ca1e020096c743eda67447492f99751bccb1a8
mcdearman <mcdearmanchris@gmail.com>
2025-09-27 12:20:29 -0500

    update

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
+import Data.Text (pack)
+import Parsifal.Parser (parseGrammar)
+import Text.Pretty.Simple (pPrint)
+import Text.Megaparsec (errorBundlePretty)
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
-
+main = do
+  contents <- readFile "examples/ungram.ug"
+  case parseGrammar (pack contents) of
+    Left err -> putStrLn $ "Error parsing grammar: " ++ errorBundlePretty err
+    Right grammar -> pPrint grammar
--- src/Parsifal/Parser.hs
+++ src/Parsifal/Parser.hs
@@ -1,43 +1,54 @@
-module Parsifal.Parser where
+module Parsifal.Parser (parseGrammar) where
 
 import Control.Applicative (Alternative ((<|>)), empty)
 import Data.Text (Text, pack)
 import Data.Void
 import Parsifal.Ungrammar
-import Text.Megaparsec (Parsec, between, many, sepBy)
+import Text.Megaparsec (MonadParsec (try), Parsec, between, many, parse, some)
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Megaparsec.Error (ParseErrorBundle)
 
 type Parser = Parsec Void Text
 
+parseGrammar :: Text -> Either (ParseErrorBundle Text Void) Grammar
+parseGrammar = parse grammar ""
+
 grammar :: Parser Grammar
-grammar = Grammar <$> many node
+grammar = Grammar <$> ((:) <$> node <*> some node)
 
 node :: Parser Node
 node = Node <$> upperIdent <* symbol "=" <*> rule
 
 rule :: Parser Rule
-rule = lexeme (tokenRule <|> nodeRule <|> sequenceRule <|> altRule <|> optRule <|> repRule <|> parens)
+rule =
+  try sequenceRule
+    <|> try optRule
+    <|> try repRule
+    <|> try atom
+    <|> try altRule
+  where
+    sequenceRule = RuleSeq <$> ((:) <$> atom <*> some atom)
+    altRule = RuleAlt <$> ((:) <$> atom <*> some (symbol "|" *> rule))
+    optRule = RuleOpt <$> atom <* symbol "?"
+    repRule = RuleRep <$> atom <* symbol "*"
+
+atom :: Parser Rule
+atom = try nodeRule <|> try tokenRule <|> parens <|> labelRule
   where
     tokenRule = RuleToken <$> token
     nodeRule = RuleNode <$> upperIdent
-    sequenceRule = RuleSeq <$> many rule
-    altRule = RuleAlt <$> sepBy rule (symbol "|")
-    optRule = RuleOpt <$> rule <* symbol "?"
-    repRule = RuleRep <$> rule <* symbol "*"
     parens = between (symbol "(") (symbol ")") rule
-
-label :: Parser Text
-label = pack <$> many L.charLiteral
+    labelRule = RuleLabeled <$> ident <* symbol ":" <*> rule
 
 token :: Parser Token
-token = Token <$> between (symbol "'") (symbol "'") ident
+token = Token <$> lexeme (between (symbol "'") (symbol "'") ident)
 
 upperIdent :: Parser Text
-upperIdent = pack <$> ((:) <$> upperChar <*> many alphaNumChar)
+upperIdent = pack <$> lexeme ((:) <$> upperChar <*> many alphaNumChar)
 
 ident :: Parser Text
-ident = pack <$> ((:) <$> letterChar <*> many alphaNumChar)
+ident = pack <$> lexeme ((:) <$> letterChar <*> many alphaNumChar)
 
 symbol :: Text -> Parser Text
 symbol = L.symbol sc
@@ -46,4 +57,7 @@ lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
 
 sc :: Parser ()
-sc = L.space space1 empty empty
+sc = L.space space1 lineComment empty
+
+lineComment :: Parser ()
+lineComment = L.skipLineComment "//"
