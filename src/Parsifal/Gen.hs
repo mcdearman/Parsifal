708a4c0af1b26cb0de4d61a69db5c421a34fb038
mcdearman <mcdearmanchris@gmail.com>
2025-09-30 17:48:34 -0400

    update

--- src/Parsifal/Gen.hs
+++ src/Parsifal/Gen.hs
@@ -1,18 +1,28 @@
 module Parsifal.Gen where
 
 import Data.Text (Text, intercalate, unpack)
+import Parsifal.Ungrammar
 
-genSyntaxKindDecls :: [Text] -> String
+collectNames :: Grammar -> Text
+collectNames g = intercalate "\n" $ map nodeName (gramNodes g) <> collectTokenNames g
+  where
+    collectTokenNames :: [Text]
+    collectTokenNames = [tokenName t | Node _ r <- gramNodes g, t <- collectTokens r]
+
+    collectTokens :: Rule -> [Token]
+    collectTokens (RuleLabeled _ r) = collectTokens r
+
+genSyntaxKindDecls :: [String] -> String
 genSyntaxKindDecls names =
   let nameMap = zip names [0 ..]
-      sigs = map (\n -> "pattern SyntaxKind" <> unpack n <> " :: SyntaxKind") names
+      sigs = map (\n -> "pattern SyntaxKind" <> n <> " :: SyntaxKind") names
       defs =
         map
           ( \(n, ix) ->
               "pattern SyntaxKind"
-                <> unpack n
+                <> n
                 <> " = SyntaxKind "
                 <> show ix
           )
           nameMap
-   in unlines $ sigs ++ defs
+   in unlines $ zipWith (\sig def -> sig <> "\n" <> def) sigs defs
