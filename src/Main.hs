cb2a6ac2e38cd6abcd028289506eefbd10be02c3
mcdearman <mcdearmanchris@gmail.com>
2025-10-26 09:48:46 -0500

    update

--- src/Main.hs
+++ src/Main.hs
@@ -8,7 +8,7 @@ import Text.Pretty.Simple (pPrint)
 
 main :: IO ()
 main = do
-  contents <- readFile "examples/rust.ug"
+  contents <- readFile "examples/lc.ug"
   case parseGrammar (pack contents) of
     Left err -> putStrLn $ "Error parsing grammar: " ++ errorBundlePretty err
     Right grammar ->
